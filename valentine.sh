8959273976ac09fdb215b1486eab8654a35562d1
sachin <thesachin@devops.localdomain>
2026-02-07 04:00:22 +0000

    added one more option below git

--- valentine.sh
+++ valentine.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 read -p "Enter your name: " name
+read -p "Enter your gender: " gender
 
 echo "Happy Valentine's Day, $name ❤️"
+echo "you are a beautiful, $gender  "
 echo "Practicing scripts is a great habit!"
 
 read -p "Do you like scripting? (yes/no): " ans
